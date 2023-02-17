a0d774577f8a78b2fbf990d710f1415399a1abca
Tylor Clont <tclont@yahoo.com>
2023-02-16 23:25:41 -0500

    Added navbar

--- meanPOS/src/app/app.module.ts
+++ meanPOS/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee/employee.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
